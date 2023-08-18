2b995c6136d6144552300ce12a8462d0fe143663
Sayan Datta <hello@sayandatta.in>
2023-08-18 15:26:48 +0530

    added

---
+++ src/FileManager.php
@@ -0,0 +1,14 @@
+<?php
+declare( strict_types=1 );
+
+namespace InstaWP\Connect\Helpers;
+
+class FileManager {
+
+    public function fetch(): array {
+        $results = [];
+		
+		
+        return $results;
+    }
+}
